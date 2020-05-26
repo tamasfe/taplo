fe3abfe06b24e14a34d562b42096e99ef46a1b4f
tamasf97 <tamasf97@outlook.com>
2020-05-26 17:19:52 +0200

    removed accidental test

--- taplo/src/tests/mod.rs
+++ taplo/src/tests/mod.rs
@@ -1,5 +1,3 @@
-use std::fs;
-
 mod generated {
     mod invalid;
     mod valid;
@@ -8,18 +6,3 @@ mod generated {
 // Requires nightly
 #[cfg(feature = "bench")]
 mod benches;
-
-// todo
-#[test]
-fn faszom() {
-    let src = fs::read_to_string("../.local/testing.toml").unwrap();
-
-    let d = crate::parser::parse(&src).into_dom();
-
-    if !d.errors().is_empty() {
-        println!("\n\nerrs:");
-        for e in d.errors() {
-            println!("{}", e);
-        }
-    }
-}

be439feb5b9190ee4a2a5075d90aaec87d06cde4
tamasf97 <tamasf97@outlook.com>
2020-07-16 01:25:56 +0200

    fix(schema): resolves #17

--- util/schema-gen/src/gen/mod.rs
+++ util/schema-gen/src/gen/mod.rs
@@ -1,4 +1,8 @@
-use schemars::schema::SchemaObject;
+use schemars::{
+    schema::{Schema, SchemaObject, SingleOrVec},
+    visit::Visitor,
+    Map,
+};
 
 pub mod cargo;
 
@@ -34,7 +38,7 @@ impl schemars::visit::Visitor for KeysVisitor {
         }
 
         KeyVisitor(keys.clone()).visit_schema_object(schema);
-        schemars::visit::visit_schema_object(&mut KeyVisitor(keys.clone()), schema);
+        visit_schema_object(&mut KeyVisitor(keys.clone()), schema);
 
         if let Some(obj) = &mut schema.object {
             for (k, s) in &mut obj.properties {
@@ -42,6 +46,67 @@ impl schemars::visit::Visitor for KeysVisitor {
                 new_keys.push(k.clone());
 
                 schemars::visit::visit_schema(&mut KeysVisitor(new_keys), s);
+            }
+        }
+    }
+}
+
+// Filtered visit
+pub fn visit_schema_object<V: schemars::visit::Visitor + ?Sized>(
+    v: &mut V,
+    schema: &mut SchemaObject,
+) {
+    if let Some(sub) = &mut schema.subschemas {
+        visit_vec(v, &mut sub.all_of);
+        visit_vec(v, &mut sub.any_of);
+        visit_vec(v, &mut sub.one_of);
+        visit_box(v, &mut sub.not);
+        visit_box(v, &mut sub.if_schema);
+        visit_box(v, &mut sub.then_schema);
+        visit_box(v, &mut sub.else_schema);
+    }
+
+    if let Some(arr) = &mut schema.array {
+        visit_single_or_vec(v, &mut arr.items);
+        visit_box(v, &mut arr.additional_items);
+        visit_box(v, &mut arr.contains);
+    }
+
+    if let Some(obj) = &mut schema.object {
+        visit_map_values(v, &mut obj.properties);
+        visit_map_values(v, &mut obj.pattern_properties);
+        // visit_box(v, &mut obj.additional_properties);
+        visit_box(v, &mut obj.property_names);
+    }
+}
+
+fn visit_box<V: Visitor + ?Sized>(v: &mut V, target: &mut Option<Box<Schema>>) {
+    if let Some(s) = target {
+        v.visit_schema(s)
+    }
+}
+
+fn visit_vec<V: Visitor + ?Sized>(v: &mut V, target: &mut Option<Vec<Schema>>) {
+    if let Some(vec) = target {
+        for s in vec {
+            v.visit_schema(s)
+        }
+    }
+}
+
+fn visit_map_values<V: Visitor + ?Sized>(v: &mut V, target: &mut Map<String, Schema>) {
+    for s in target.values_mut() {
+        v.visit_schema(s)
+    }
+}
+
+fn visit_single_or_vec<V: Visitor + ?Sized>(v: &mut V, target: &mut Option<SingleOrVec<Schema>>) {
+    match target {
+        None => {}
+        Some(SingleOrVec::Single(s)) => v.visit_schema(s),
+        Some(SingleOrVec::Vec(vec)) => {
+            for s in vec {
+                v.visit_schema(s)
             }
         }
     }

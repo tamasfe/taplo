1d8ae1e1dfa62d91244cd9e1f53144051da6aa7b
Ferenc Tamás <tamasf97@outlook.com>
2020-11-04 23:15:42 +0100

    misc: changed bench toml file

--- taplo/benches/benches.rs
+++ taplo/benches/benches.rs
@@ -7,18 +7,18 @@ use test::Bencher;
 
 #[bench]
 fn bench_taplo_parse_validate(b: &mut Bencher) {
-    let src = fs::read_to_string("../taplo-ide/vscode/sample/example-v0.4.0.toml").unwrap();
+    let src = fs::read_to_string("../test-data/example.toml").unwrap();
     b.iter(|| taplo::parser::parse(&src).into_dom());
 }
 
 #[bench]
 fn bench_taplo_parse(b: &mut Bencher) {
-    let src = fs::read_to_string("../taplo-ide/vscode/sample/example-v0.4.0.toml").unwrap();
+    let src = fs::read_to_string("../test-data/example.toml").unwrap();
     b.iter(|| taplo::parser::parse(&src));
 }
 
 #[bench]
 fn bench_toml_rs(b: &mut Bencher) {
-    let src = fs::read_to_string("../taplo-ide/vscode/sample/example-v0.4.0.toml").unwrap();
+    let src = fs::read_to_string("../test-data/example.toml").unwrap();
     b.iter(|| toml::from_str::<toml::Value>(&src).unwrap());
 }

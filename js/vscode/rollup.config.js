94f0e589c0556bf8a0f7cf94a1943d3fa5b15412
Ferenc Tamás <tamasf97@outlook.com>
2021-03-03 16:28:03 +0100

    chore(vscode): removed unused dependency

--- js/vscode/rollup.config.js
+++ js/vscode/rollup.config.js
@@ -1,4 +1,3 @@
-import rust from "@wasm-tool/rollup-plugin-rust";
 import typescript from "rollup-plugin-typescript2";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
@@ -14,11 +13,6 @@ export default {
     dir: "dist",
   },
   plugins: [
-    rust({
-      debug: process.env["RELEASE"] !== "true",
-      nodejs: true,
-      inlineWasm: process.env["SEPARATE_WASM"] !== "true",
-    }),
     typescript(),
     commonjs({ include: ["src/*.ts", "node_modules/**", "../lsp/**"] }),
     resolve({ jsnext: true, preferBuiltins: true }),
